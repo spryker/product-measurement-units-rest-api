f2433da624e5579b9114c9027087d28ec2fe2e23
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/ProductMeasurementUnitsRestApi/ProductMeasurementUnitsRestApiConfig.php
+++ src/Spryker/Glue/ProductMeasurementUnitsRestApi/ProductMeasurementUnitsRestApiConfig.php
@@ -12,16 +12,22 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_PRODUCT_MEASUREMENT_UNITS = 'product-measurement-units';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_SALES_UNITS = 'sales-units';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\ProductsRestApi\ProductsRestApiConfig::RESOURCE_CONCRETE_PRODUCTS
      *
      * @var string
@@ -29,6 +35,8 @@ class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_CONCRETE_PRODUCTS = 'concrete-products';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\ProductsRestApi\ProductsRestApiConfig::RESPONSE_CODE_CANT_FIND_CONCRETE_PRODUCT
      *
      * @var string
@@ -36,6 +44,8 @@ class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_CODE_CANT_FIND_CONCRETE_PRODUCT = '302';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\ProductsRestApi\ProductsRestApiConfig::RESPONSE_DETAIL_CANT_FIND_CONCRETE_PRODUCT
      *
      * @var string
@@ -43,6 +53,8 @@ class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_DETAIL_CANT_FIND_CONCRETE_PRODUCT = 'Concrete product is not found.';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\ProductsRestApi\ProductsRestApiConfig::RESPONSE_CODE_CONCRETE_PRODUCT_SKU_IS_NOT_SPECIFIED
      *
      * @var string
@@ -50,6 +62,8 @@ class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_CODE_CONCRETE_PRODUCT_SKU_IS_NOT_SPECIFIED = '312';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\ProductsRestApi\ProductsRestApiConfig::RESPONSE_DETAIL_CONCRETE_PRODUCT_SKU_IS_NOT_SPECIFIED
      *
      * @var string
@@ -57,21 +71,29 @@ class ProductMeasurementUnitsRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_DETAIL_CONCRETE_PRODUCT_SKU_IS_NOT_SPECIFIED = 'Concrete product sku is not specified.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PRODUCT_MEASUREMENT_UNIT_CODE_IS_NOT_SPECIFIED = '3401';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PRODUCT_MEASUREMENT_UNIT_NOT_FOUND = '3402';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_MEASUREMENT_UNIT_CODE_IS_NOT_SPECIFIED = 'Product measurement unit code has not been specified.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_PRODUCT_MEASUREMENT_UNIT_NOT_FOUND = 'Product measurement unit not found.';
